1419e2e1dd63aa30338263e90d1de1946e9b1010
mizmizsan2 <hirario0615@gmail.com>
2022-04-30 21:58:38 +0900

    スコア系追加

--- www/sketch.js
+++ www/sketch.js
@@ -20,13 +20,26 @@ class object {
         ellipse(this.x1, this.y1, this.s, this.s);
         this.x1 += this.mx1;
         this.y1 += this.my1;
-        if (this.x1 <= -50 || this.x1 >= 420) {
+        if (this.x1 <= -50) {
+            this.x1 = 420;
+            this.y1 = random(-50, 830);
+            this.mx1 = random(-4, 4);
+            this.my1 = random(-4, 4);
+        }
+        if (this.x1 >= 420) {
             this.x1 = -50;
             this.y1 = random(-50, 830);
             this.mx1 = random(-4, 4);
             this.my1 = random(-4, 4);
         }
-        if (this.y1 <= -50 || this.y1 >= 830) {
+
+        if (this.y1 < -50) {
+            this.x1 = random(-50, 420);
+            this.y1 = 830;
+            this.mx1 = random(-4, 4);
+            this.my1 = random(-4, 4);
+        }
+        if (this.y1 > 830) {
             this.x1 = random(-50, 420);
             this.y1 = -50;
             this.mx1 = random(-4, 4);
@@ -40,13 +53,26 @@ class object {
         ellipse(this.x2, this.y2, this.s, this.s);
         this.x2 += this.mx2;
         this.y2 += this.my2;
-        if (this.x2 <= -50 || this.x2 >= 420) {
+        if (this.x2 < -50) {
+            this.x2 = 420;
+            this.y2 = random(-50, 830);
+            this.mx2 = random(-4, 4);
+            this.my2 = random(-4, 4);
+        }
+        if (this.x2 > 420) {
             this.x2 = -50;
             this.y2 = random(-50, 830);
             this.mx2 = random(-4, 4);
             this.my2 = random(-4, 4);
         }
-        if (this.y2 <= -50 || this.y2 >= 830) {
+
+        if (this.y2 < -50) {
+            this.x2 = random(-50, 420);
+            this.y2 = 830;
+            this.mx2 = random(-4, 4);
+            this.my2 = random(-4, 4);
+        }
+        if (this.y2 > 830) {
             this.x2 = random(-50, 420);
             this.y2 = -50;
             this.mx2 = random(-4, 4);
@@ -74,6 +100,8 @@ function setup() {
     createCanvas(390, 800);
     background(0);
 
+    scoreResult = 0;
+
 
 }
 
@@ -91,6 +119,7 @@ function draw() {
         if (c1[i].del() == 1 && mouseIsPressed) {
             c1.splice(i, 1);
             console.log('up');
+            scoreCal(10);
         }
     }
 
@@ -100,9 +129,18 @@ function draw() {
         if (c1[i].del() == 2 && mouseIsPressed) {
             c1.splice(i, 1);
             console.log('down');
+            scoreCal(-10);
         }
     }
-    // }
-
+    scoreText();
 }
 
+function scoreCal(score){
+    scoreResult += score;
+}
+
+function scoreText(){
+    fill(0);
+    textSize(24);
+    text("score→"+scoreResult, 10 ,30);
+}
